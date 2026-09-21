e9e8d09cc11c531590574816bfb977d57a04b575
Halolro <shg0205123131@gmail.com>
2026-09-21 17:58:43 +0900

    Revert "배경 이미지 변경"
    This reverts commit 4ee23941cc16e5b13423165842ef23661583b6f2.

--- Assets/00.Script/MainScreenUI.cs
+++ Assets/00.Script/MainScreenUI.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 public class MenuCameraController : MonoBehaviour
 {
     [Header("카메라 설정")]
-    public float duration = 2.0f;
+    public float duration = 1.5f;
 
     [Header("UI 설정")]
     public Button startButton;
