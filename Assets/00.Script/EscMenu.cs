2384340e30bff18c68f4b21407dafe440a266bfa
Halolro <shg0205123131@gmail.com>
2026-09-23 14:00:20 +0900

    Ui간단 수정
    Esc키 눌러서 껏다키는거 만들었습니다

--- Assets/00.Script/EscMenu.cs
+++ Assets/00.Script/EscMenu.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class EscMenu : MonoBehaviour
 {
+    public GameObject menu;
+    public GameObject Sound;
+    private bool isEsc;
     void Start()
     {
-        
+        menu.SetActive(false);
+        isEsc = false;
     }
 
     void Update()
     {
-        
+        if (Keyboard.current.escapeKey.wasPressedThisFrame)
+        {
+            isEsc = !isEsc;
+            menu.SetActive(isEsc);
+        }
+    }
+
+    public void ClickSoundPanel()
+    {
+        Sound.SetActive(true);
+        isEsc = !isEsc;
+        menu.SetActive(isEsc);
     }
 }
